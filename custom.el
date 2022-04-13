50922b074dc314e9d359467e5598343d5af00ba2
Jack <liangxiaoming@qing5.com>
2022-04-13 15:50:02 +0800

    - 去掉format-all

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(super-save restclient quickrun yaml-mode protobuf-mode markdown-mode json-mode typescript-mode web-mode emmet-mode rust-mode go-mode eglot yasnippet-snippets yasnippet which-key highlight-parentheses rainbow-delimiters popwin org-superstar neotree move-dup info-colors iedit gnu-elpa-keyring-update format-all exec-path-from-shell crux ctrlf company auto-package-update delight diminish use-package)))
+   '(super-save restclient quickrun yaml-mode protobuf-mode markdown-mode json-mode typescript-mode web-mode emmet-mode rust-mode go-mode eglot yasnippet-snippets yasnippet which-key highlight-parentheses rainbow-delimiters popwin org-superstar neotree move-dup info-colors iedit gnu-elpa-keyring-update exec-path-from-shell crux ctrlf company auto-package-update delight diminish use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
