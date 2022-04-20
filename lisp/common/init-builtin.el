c533bfba2eff9f7af1f07a09df18fb6b5d0bb546
Jack <liangxiaoming@qing5.com>
2022-04-20 09:57:49 +0800

    * 修改主题，修改近期打开文件数量限制

--- lisp/common/init-builtin.el
+++ lisp/common/init-builtin.el
@@ -79,8 +79,8 @@
   :hook (after-init . recentf-mode)
   :bind (("C-c r" . #'recentf-open-files))
   :config
-  (setq-default recentf-max-menu-items 50
-                recentf-max-saved-items 100)
+  (setq-default recentf-max-menu-items 20
+                recentf-max-saved-items 20)
   (add-to-list 'recentf-exclude '("~\/.emacs.d\/elpa\/")))
 
 ;; global visual line mode
