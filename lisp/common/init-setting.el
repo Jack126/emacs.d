a38ab810d8cb5cacc81adf94e9ea10f86dbcbcc1
Jack <liangxiaoming@qing5.com>
2022-04-22 10:35:08 +0800

    + 添加注释快捷键

--- lisp/common/init-setting.el
+++ lisp/common/init-setting.el
@@ -20,6 +20,17 @@
      (line-end-position)))
      (message "1 line copied"))
 
+;; 添加行注释
+(defun qiang-comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command. If no region is selected and current line is not blank and we are not at the end of the line, then comment current line. Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+
+
+(global-set-key (kbd "M-;") 'qiang-comment-dwim-line) 
 (global-set-key (kbd "M-q") 'query-replace)
 (global-set-key (kbd "C-c .") 'now) ;; now time
 (global-set-key (kbd "C-c ,") 'user) ;; user
