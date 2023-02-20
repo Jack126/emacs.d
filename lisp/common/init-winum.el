ebdf80207e3ffc3b9ca1bccc53081cb1c594f040
Jack <liangxiaoming@qing5.com>
2023-02-20 17:27:46 +0800

    * 调换窗口位置

--- lisp/common/init-winum.el
+++ lisp/common/init-winum.el
@@ -24,6 +24,30 @@
 (defun winum-assign-0-to-neotree ()
   (when (string-match-p (buffer-name) ".*\\*NeoTree\\*.*") 10))
 (add-to-list 'winum-assign-functions #'winum-assign-0-to-neotree)
+
+(defun rotate-windows (arg)
+  "Rotate your windows; use the prefix argument to rotate the other direction"
+  (interactive "P")
+  (if (not (> (count-windows) 1))
+      (message "You can't rotate a single window!")
+    (let* ((rotate-times (prefix-numeric-value arg))
+           (direction (if (or (< rotate-times 0) (equal arg '(4)))
+                          'reverse 'identity)))
+      (dotimes (_ (abs rotate-times))
+        (dotimes (i (- (count-windows) 1))
+          (let* ((w1 (elt (funcall direction (window-list)) i))
+                 (w2 (elt (funcall direction (window-list)) (+ i 1)))
+                 (b1 (window-buffer w1))
+                 (b2 (window-buffer w2))
+                 (s1 (window-start w1))
+                 (s2 (window-start w2))
+                 (p1 (window-point w1))
+                 (p2 (window-point w2)))
+            (set-window-buffer-start-and-point w1 b2 s2 p2)
+            (set-window-buffer-start-and-point w2 b1 s1 p1)))))))
+
+(define-key map (kbd "M-]") 'rotate-windows)
+
 (provide 'init-winum)
 ;;; init-fn.el ends here
 ;; Local Variables:
