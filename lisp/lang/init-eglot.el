b338fb35db92e23a5096cda8a2180c457b795a2b
Jack <liangxiaoming@qing5.com>
2024-05-24 16:50:18 +0800

    add eglot phpactor

--- lisp/lang/init-eglot.el
+++ lisp/lang/init-eglot.el
@@ -28,7 +28,7 @@
     (set-face-attribute 'eglot-highlight-symbol-face nil
                         :background "#b3d7ff"))
   ;; (setq eglot-ignored-server-capabilities '(:documentHighlightProvider))
-  (add-to-list 'eglot-server-programs '(web-mode "vls"))
+  (add-to-list 'eglot-server-programs '((web-mode "vls") (php-mode "phpactor" "language-server")))
     ;; (defun eglot-actions-before-save()
     ;;   (add-hook 'before-save-hook #'eglot-format-buffer))
     ;; (add-hook 'eglot--managed-mode-hook #'eglot-actions-before-save)
