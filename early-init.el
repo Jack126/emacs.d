259b0ad23db61ab914aee9242087a89d3a05bd41
Jack <liangxiaoming@qing5.com>
2024-10-22 16:26:46 +0800

    * 修正 doom-one主题导致blink-search 搜索失败问题

--- early-init.el
+++ early-init.el
@@ -35,8 +35,8 @@
 ;; System default coding
 (set-language-environment 'utf-8)
 
-;;(add-to-list 'custom-theme-load-path "~/.emacs.d/lisp/themes/")
-;;(load-theme 'zenburn t)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/lisp/themes/")
+(load-theme 'zenburn t)
 ;;(load-theme 'misterioso t)
 
 ;; start time
